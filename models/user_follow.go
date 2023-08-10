5d3a65556d8a6d9196fabe2d8cebff7d3da411bd
lixiao985 <1753123340@qq.com>
2023-08-11 00:23:18 +0800

    add user 关注表

--- models/user_follow.go
+++ models/user_follow.go
@@ -1,11 +1,11 @@
 package models
 
-type user_follow struct {
+type User_follow struct {
 	// 用户的关注信息
 	User_Id   uint `gorm:"primaryKey;not null"` // 用户id
 	Follow_Id uint `gorm:"not null"`            // 关注用户id
 }
 
-func (*user_follow) TableName() string {
+func (*User_follow) TableName() string {
 	return "user_follow"
 }
