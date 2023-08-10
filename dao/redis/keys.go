25505681b90e9f6e93e03189e2fd5d01f8beb2c5
lixiao985 <1753123340@qq.com>
2023-08-11 01:06:55 +0800

    add user关注表的redis操作

--- dao/redis/keys.go
+++ dao/redis/keys.go
@@ -3,3 +3,5 @@ package redis
 const Video = "video_"
 
 const VideoPage = "video_page_"
+
+const User = "user_"
---
+++ dao/redis/user_follow.go
@@ -0,0 +1,70 @@
+package redis
+
+import (
+	"fmt"
+	_ "github.com/redis/go-redis/v9"
+	"strconv"
+)
+
+const (
+	KeyFollowCount   = "follow_count"
+	KeyFollowerCount = "follower_count  "
+)
+
+// 根据userId查找关注数
+func GetFollowCountById(userId uint) (int, error) {
+	key := User + fmt.Sprintf("%d", userId)
+	count, err := Rdb.HGet(Ctx, key, KeyFollowCount).Result()
+	commentCount, _ := strconv.Atoi(count)
+	return commentCount, err
+}
+
+// 根据userId查找粉丝数
+func GetFollowerCountById(userId uint) (int, error) {
+	key := User + fmt.Sprintf("%d", userId)
+	count, err := Rdb.HGet(Ctx, key, KeyFollowerCount).Result()
+	commentCount, _ := strconv.Atoi(count)
+	return commentCount, err
+}
+
+// 给Id对应的关注数加一
+func IncreaseFollowCountByUserId(userId uint) error {
+	key := Video + fmt.Sprintf("%d", userId)
+	_, err := Rdb.HIncrBy(Ctx, key, KeyFollowCount, 1).Result()
+	return err
+}
+
+// 给Id对应的关注数减一
+func DecreaseFollowCountByUserId(userId uint) error {
+	key := Video + fmt.Sprintf("%d", userId)
+	_, err := Rdb.HIncrBy(Ctx, key, KeyFollowCount, -1).Result()
+	return err
+}
+
+// 给videoId对应的粉丝数加一
+func IncreaseFollowerCountByUserId(userId uint) error {
+	key := Video + fmt.Sprintf("%d", userId)
+	_, err := Rdb.HIncrBy(Ctx, key, KeyFollowerCount, 1).Result()
+	return err
+}
+
+// 给videoId对应的粉丝数减一
+func DecreaseFollowerCountByUserId(userId uint) error {
+	key := Video + fmt.Sprintf("%d", userId)
+	_, err := Rdb.HIncrBy(Ctx, key, KeyFollowerCount, -1).Result()
+	return err
+}
+
+// 设置关注数
+func SetFollowCountByVideoId(userid uint, count int64) error {
+	key := Video + fmt.Sprintf("%d", userid)
+	err := Rdb.HSet(Ctx, key, KeyFollowCount, count).Err()
+	return err
+}
+
+// 设置粉丝数
+func SetFollowerCountByVideoId(userid uint, count int64) error {
+	key := Video + fmt.Sprintf("%d", userid)
+	err := Rdb.HSet(Ctx, key, KeyFollowerCount, count).Err()
+	return err
+}
