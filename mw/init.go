7152a3a79ad80d43994e2cee377e861986f1aba0
licheng <li1253767085@gmail.com>
2023-08-17 10:21:06 +0800

    update redis和kafka不再统一全部进行初始化，各个模块根据自己的需要调用初始化函数

--- mw/init.go
+++ mw/init.go
@@ -2,19 +2,8 @@ package mw
 
 import (
 	"douyin/config"
-	"douyin/mw/kafka"
-	"douyin/mw/redis"
 )
 
 func Init(appConfig *config.AppConfig) error {
-	err := kafka.Init(appConfig)
-	if err != nil {
-		return err
-	}
-
-	err = redis.Init(appConfig)
-	if err != nil {
-		return err
-	}
 	return nil
 }
