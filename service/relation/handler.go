8695919c2ff625157375dc777fc4a07159f04057
lixiao985 <1753123340@qq.com>
2023-08-17 18:17:13 +0800

    add 延迟双删，checkandset redis键

--- service/relation/handler.go
+++ service/relation/handler.go
@@ -2,19 +2,98 @@ package main
 
 import (
 	"context"
+	"douyin/constant"
 	"douyin/dal/mysql"
 	relation "douyin/kitex_gen/relation"
+	"douyin/kitex_gen/user/userservice"
 	"douyin/mw/redis"
+	"github.com/apache/thrift/lib/go/thrift"
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/kitex-contrib/obs-opentelemetry/tracing"
+	etcd "github.com/kitex-contrib/registry-etcd"
 	"go.uber.org/zap"
+	"log"
+	"time"
 )
+
+var userClient userservice.Client
+
+func init() {
+	// Etcd 服务发现
+	r, err := etcd.NewEtcdResolver([]string{constant.EtcdAddr})
+	if err != nil {
+		log.Fatal(err)
+	}
+	userClient, err = userservice.NewClient(
+		constant.UserServiceName,
+		client.WithResolver(r),
+		client.WithSuite(tracing.NewClientSuite()),
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: constant.CommentServiceName}))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 // RelationServiceImpl implements the last service interface defined in the IDL.
 type RelationServiceImpl struct{}
 
 // RelationAction implements the RelationServiceImpl interface.
 func (s *RelationServiceImpl) RelationAction(ctx context.Context, request *relation.RelationActionRequest) (resp *relation.RelationActionResponse, err error) {
-	// TODO: Your code here...
-	return
+	resp = new(relation.RelationActionResponse)
+	zap.L().Info("RelationClient action start",
+		zap.Int32("user_id", request.UserId),
+		zap.Int32("action_type", request.ActionType),
+		zap.Int32("ToUserId", request.ToUserId),
+	)
+
+	switch request.ActionType {
+	case 1: // 关注
+		// 延迟双删
+		redis.DelKey(uint(request.UserId), redis.FollowList)
+		redis.DelKey(uint(request.ToUserId), redis.FollowerList)
+
+		err := mysql.AddFollow(uint(request.UserId), uint(request.ToUserId))
+		if err != nil {
+			resp.StatusCode = 1
+			resp.StatusMsg = thrift.StringPtr(err.Error())
+			return
+		}
+
+		time.Sleep(1 * time.Second)
+
+		redis.DelKey(uint(request.UserId), redis.FollowList)
+		redis.DelKey(uint(request.ToUserId), redis.FollowerList)
+
+		return &relation.RelationActionResponse{
+			StatusCode: 0,
+			StatusMsg:  thrift.StringPtr("success"),
+		}, nil
+	case 2: // 取关
+		// 延迟双删
+		redis.DelKey(uint(request.UserId), redis.FollowList)
+		redis.DelKey(uint(request.ToUserId), redis.FollowerList)
+
+		err := mysql.DeleteFollowById(uint(request.UserId), uint(request.ToUserId))
+		if err != nil {
+			resp.StatusCode = 1
+			resp.StatusMsg = thrift.StringPtr(err.Error())
+			return
+		}
+
+		time.Sleep(1 * time.Second)
+
+		redis.DelKey(uint(request.UserId), redis.FollowList)
+		redis.DelKey(uint(request.ToUserId), redis.FollowerList)
+
+		return &relation.RelationActionResponse{
+			StatusCode: 0,
+			StatusMsg:  thrift.StringPtr("success"),
+		}, nil
+
+	default:
+		return
+	}
 }
 
 // GetFollowList implements the RelationServiceImpl interface.
@@ -71,4 +150,33 @@ func (s *RelationServiceImpl) IsFollowing(ctx context.Context, request *relation
 		}
 	}()
 	return &relation.IsFollowingResponse{Result_: found}, err
+}
+
+// 返回true表示不存在这个key，并设置key
+// 返回false表示已存在这个key
+func CheckAndSetRedisRRealationKey(userId uint, key string) bool {
+	if redis.IsExistUserSetField(userId, key) {
+		return false
+	} else {
+		if key == redis.FollowList {
+			id, err := mysql.GetFollowList(userId)
+			if err != nil {
+				log.Println("mysql获取FollowList失败", err)
+			}
+			err = redis.SetFollowListByUserId(userId, id)
+			if err != nil {
+				log.Println("redis更新FollowList失败", err)
+			}
+		} else {
+			id, err := mysql.GetFollowerList(userId)
+			if err != nil {
+				log.Println("mysql获取FollowerList失败", err)
+			}
+			err = redis.SetFollowerListByUserId(userId, id)
+			if err != nil {
+				log.Println("redis更新FollowerList失败", err)
+			}
+		}
+		return true
+	}
 }
