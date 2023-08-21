f9d12f33a65cd49cc516b48a2a88c737bd9603c6
lixiao985 <1753123340@qq.com>
2023-08-21 19:05:30 +0800

    update relation model , add id

--- dal/model/relation.go
+++ dal/model/relation.go
@@ -2,8 +2,9 @@ package model
 
 type UserFollow struct {
 	// 用户的关注信息
-	UserId   uint `gorm:"primaryKey;index;not null"` // 用户id
-	FollowId uint `gorm:"primaryKey;index;not null"` // 关注用户id
+	ID       int  `gorm:"primaryKey"`
+	UserId   uint `gorm:"index;not null"` // 用户id
+	FollowId uint `gorm:"index;not null"` // 关注用户id
 }
 
 func (*UserFollow) TableName() string {
